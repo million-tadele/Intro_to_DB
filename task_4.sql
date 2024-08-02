1af70a1fc9d7e1a7bed1e8d70be7062524d91723
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-08-02 06:29:58 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,11 +1,16 @@
 USE alx_book_store;
 
 SELECT 
-    COLUMN_NAME AS 'Column Name', 
-    COLUMN_TYPE AS 'Column Type', 
-    IS_NULLABLE AS 'Is Nullable', 
-    COLUMN_KEY AS 'Key', 
-    COLUMN_DEFAULT AS 'Default', 
-    EXTRA AS 'Extra'
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'books';
+    c.COLUMN_NAME AS 'Column Name', 
+    c.COLUMN_TYPE AS 'Column Type', 
+    c.IS_NULLABLE AS 'Is Nullable', 
+    c.COLUMN_KEY AS 'Key', 
+    c.COLUMN_DEFAULT AS 'Default', 
+    c.EXTRA AS 'Extra'
+FROM 
+    (SELECT * FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = 'books') t
+JOIN 
+    information_schema.columns c ON t.table_name = c.table_name
+WHERE 
+    t.table_schema = DATABASE();
+
